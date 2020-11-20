240372e4b0899dfd01a4d5511f0a9c630e508932
Jose David Lugo <jose.lugo@dinmax.com>
2020-11-20 14:14:11 -0400

    update routes connection

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('layouts/app');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    return redirect('atencion');
+});
